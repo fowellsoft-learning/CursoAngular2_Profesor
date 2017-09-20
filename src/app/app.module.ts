b256cb23d862bc34a7db775f59f79a432ddc61fd
formacion <formacion@ANSFORM05.indra.es>
2017-09-20 15:02:32 +0200

    Personas - Formularios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { PersonasViewModelService } from './personas/personas-view-model.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -10,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { DemosComponent } from './demos/demos.component';
 import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { SizerComponent } from './sizer/sizer.component';
+import { PersonasComponent } from './personas/personas.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +20,12 @@ import { SizerComponent } from './sizer/sizer.component';
     DemosComponent,
     CalculadoraComponent,
     SizerComponent,
+    PersonasComponent,
    ],
   imports: [
     BrowserModule, FormsModule, MyCoreModule,
   ],
-  providers: [ LoggerService, MisDatosService ],
+  providers: [ LoggerService, MisDatosService, PersonasViewModelService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/personas/personas-view-model.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { LoggerService } from '../../my-core/index';
+
+@Injectable()
+export class PersonasViewModelService {
+  private listado: Array<any> = new Array<any>();
+  private elemento: any = {};
+  private modo: 'list' | 'add' | 'edit' | 'delete' | 'view' = 'list';
+  private idOriginal: number = null;
+  private PK = 'id';
+
+  constructor(private log: LoggerService) { }
+
+  get Listado() { return this.listado; }
+  get Elemento() { return this.elemento; }
+  get Modo () { return this.modo; }
+
+  private load() {
+    if (this.listado.length === 0) {
+      this.listado = [
+        {id: 1, nombre: 'Carmelo', apellidos: 'Coton', edad: 34},
+        {id: 2, nombre: 'Pepito', apellidos: 'Grillo', edad: 105},
+        {id: 3, nombre: 'Pedro', apellidos: 'Pica Piedra', edad: 55},
+        {id: 4, nombre: 'Capitan', apellidos: 'Tan', edad: 18},
+      ];
+    }
+  }
+
+  list() {
+    this.load();
+    this.modo = 'list';
+  }
+
+  add() {
+    this.elemento = {};
+    this.modo = 'add';
+  }
+
+  edit(id: number) {
+    let rslt = this.listado.find(ele => ele[this.PK] == id);
+    if (rslt) {
+      this.elemento = Object.assign({}, rslt);
+      this.idOriginal = this.elemento[this.PK];
+      this.modo = 'edit';
+    } else {
+      this.log.error('Elemento no encontrado.');
+    }
+  }
+
+  view(id: number) {
+    let rslt = this.listado.find(ele => ele[this.PK] == id);
+    if (rslt) {
+      this.elemento = Object.assign({}, rslt);
+      this.modo = 'view';
+    } else {
+      this.log.error('Elemento no encontrado.');
+    }
+  }
+
+  delete(id: number) {
+    if (!window.confirm('¿Seguro?')) { return; }
+    let ind = this.listado.findIndex(ele => ele[this.PK] == id);
+    if (ind) {
+      this.listado.splice(ind, 1);
+      this.list();
+    } else {
+      this.log.error('Elemento no encontrado.');
+    }
+  }
+
+  cancel() {
+    this.elemento = {};
+    this.idOriginal = null;
+    this.list();
+  }
+
+  send() {
+
+  }
+}
