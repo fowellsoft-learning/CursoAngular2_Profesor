8fbcee1ea71795fde54ea1ca28267db8cd7a26b3
formacion <formacion@ANSFORM05.indra.es>
2017-09-21 13:38:08 +0200

    Validaciones personalizadas

---
+++ src/my-core/directives/validaciones.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ValidacionesDirective } from './validaciones.directive';
+
+describe('ValidacionesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ValidacionesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
